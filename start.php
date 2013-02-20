1ad01c2f67493748273ea1fe4db3ada46d700a1c
Roj Vroemen <roj.vroemen@azion.nl>
2013-02-20 16:54:12 +0100

    Added some more comments

--- start.php
+++ start.php
@@ -6,6 +6,16 @@ Autoloader::namespaces(array(
 
 Autoloader::alias('TaskQueue\\Queues', 'Queues');
 
+/*
+|--------------------------------------------------------------------------
+| Register Queues to the IoC container
+|--------------------------------------------------------------------------
+|
+| Register Queues to the IoC container for easy access and use. We
+| register it as a singleton since there is no need to have multiple
+| instances at the same time.
+|
+*/
 IoC::singleton('Queues', function()
 {
 	$repository = new TaskQueue\Repositories\FluentQueueRepository;
@@ -13,6 +23,15 @@ IoC::singleton('Queues', function()
 	return new TaskQueue\Queues($repository);
 });
 
+/*
+|--------------------------------------------------------------------------
+| Add the routes
+|--------------------------------------------------------------------------
+|
+| Adds the routes that handle the queues. Easily accessible so it can be
+| run asynchronous and so it doesn't depend on being accessed by artisan.
+|
+*/
 Route::get(Bundle::get('task-queue.handles'), function()
 {
 	$queues = IoC::resolve('Queues');
